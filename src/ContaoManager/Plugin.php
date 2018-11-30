ContaoBlackForest/contao-news-tags-bundle

Provide the autoloading with the manager from contao

src/ContaoManager/Plugin.php
@@ -1,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-news-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-news-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-news-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace BlackForest\Contao\News\Tags\ContaoManager;
+
+use BlackForest\Contao\News\Tags\BlackForestContaoNewsTagsBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\NewsBundle\ContaoNewsBundle;
+
+/**
+ * Contao Manager plugin.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(BlackForestContaoNewsTagsBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                        ContaoNewsBundle::class
+                    ]
+                )
+        ];
+    }
+}

tests/Test/ContaoManager/PluginTest.php
@@ -1,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-news-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-news-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-news-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace BlackForest\Contao\News\Tags\Test\ContaoManager;
+
+use BlackForest\Contao\News\Tags\BlackForestContaoNewsTagsBundle;
+use BlackForest\Contao\News\Tags\ContaoManager\Plugin;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\NewsBundle\ContaoNewsBundle;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PluginTest
+ */
+class PluginTest extends TestCase
+{
+    /**
+     * Test get bundles.
+     *
+     * @cover Plugin::getBundles
+     */
+    public function testGetBundles()
+    {
+        $plugin = new Plugin();
+        $parser = $this->getMockBuilder(ParserInterface::class)->getMock();
+
+        $bundleConfig1 = BundleConfig::create(BlackForestContaoNewsTagsBundle::class)
+            ->setLoadAfter(
+                [
+                    ContaoCoreBundle::class,
+                    ContaoNewsBundle::class
+                ]
+            );
+
+        $this->assertArraySubset($plugin->getBundles($parser), [$bundleConfig1]);
+    }
+}

ContaoBlackForest/contao-calendar-tags-bundle

src/ContaoManager/Plugin.php
@@ -1,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-calendar-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-calendar-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-calendar-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace BlackForest\Contao\Calendar\Tags\ContaoManager;
+
+use BlackForest\Contao\Calendar\Tags\BlackForestContaoCalendarTagsBundle;
+use Contao\CalendarBundle\ContaoCalendarBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Contao Manager plugin.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(BlackForestContaoCalendarTagsBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                        ContaoCalendarBundle::class
+                    ]
+                )
+        ];
+    }
+}

tests/Test/ContaoManager/PluginTest.php
@@ -1,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-calendar-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-calendar-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-calendar-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace BlackForest\Contao\Calendar\Tags\Test\ContaoManager;
+
+use BlackForest\Contao\Calendar\Tags\BlackForestContaoCalendarTagsBundle;
+use BlackForest\Contao\Calendar\Tags\ContaoManager\Plugin;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\NewsBundle\ContaoNewsBundle;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PluginTest
+ */
+class PluginTest extends TestCase
+{
+    /**
+     * Test get bundles.
+     *
+     * @cover Plugin::getBundles
+     */
+    public function testGetBundles()
+    {
+        $plugin = new Plugin();
+        $parser = $this->getMockBuilder(ParserInterface::class)->getMock();
+
+        $bundleConfig1 = BundleConfig::create(BlackForestContaoCalendarTagsBundle::class)
+            ->setLoadAfter(
+                [
+                    ContaoCoreBundle::class,
+                    ContaoNewsBundle::class
+                ]
+            );
+
+        $this->assertArraySubset($plugin->getBundles($parser), [$bundleConfig1]);
+    }
+}