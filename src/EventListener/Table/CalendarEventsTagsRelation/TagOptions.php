ContaoBlackForest/contao-news-tags-bundle

Add the news tags relation backend module

src/EventListener/Table/NewsTagsRelation/TagOptions.php
@@ -1,0 +1,80 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-news-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-news-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-news-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace BlackForest\Contao\News\Tags\EventListener\Table\NewsTagsRelation;
+
+use Contao\DataContainer;
+use Doctrine\DBAL\Connection;
+
+/**
+ * This class handle the tag options.
+ */
+class TagOptions
+{
+    /**
+     * The database connection.
+     *
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * The constructor.
+     *
+     * @param Connection $connection The database connection.
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Handle the tag options.
+     *
+     * @param DataContainer $container The data container.
+     *
+     * @return array
+     */
+    public function handle(DataContainer $container)
+    {
+        if (!$container->activeRecord->archive || !$container->activeRecord->news) {
+            return [];
+        }
+
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder
+            ->select('nt.id, nt.title')
+            ->from('tl_news_tags', 'nt')
+            ->where($queryBuilder->expr()->like('nt.archives', ':archives'))
+            ->setParameter(':archives', '%"' . $container->activeRecord->archive . '"%')
+            ->orderBy('nt.title');
+
+        $statement = $queryBuilder->execute();
+        if (!$statement->rowCount()) {
+            return [];
+        }
+
+        $options = [];
+        foreach ($statement->fetchAll(\PDO::FETCH_OBJ) as $tag) {
+            $options[$tag->id] = $tag->title;
+        }
+
+        return $options;
+    }
+}

src/Resources/contao/dca/tl_user.php
@@ -22,7 +22,11 @@
  */
 
 Contao\CoreBundle\DataContainer\PaletteManipulator::create()
-    ->addField(['newstagsp'], 'news_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    ->addField(
+        ['newstagsp', 'newstagsrelationp'],
+        'news_legend',
+        Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND
+    )
     ->applyToPalette('extend', 'tl_user')
     ->applyToPalette('custom', 'tl_user');
 
@@ -30,8 +34,17 @@ Contao\CoreBundle\DataContainer\PaletteManipulator::create()
  * Add fields to tl_user_group
  */
 
-$GLOBALS['TL_DCA']['tl_user']['fields']['newstagsp'] = [
+$GLOBALS['TL_DCA']['tl_user']['fields']['newstagsp']         = [
     'label'     => &$GLOBALS['TL_LANG']['tl_user']['newstagsp'],
+    'exclude'   => true,
+    'inputType' => 'checkbox',
+    'options'   => ['create', 'delete'],
+    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+    'eval'      => ['multiple' => true],
+    'sql'       => 'blob NULL'
+];
+$GLOBALS['TL_DCA']['tl_user']['fields']['newstagsrelationp'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_user']['newstagsrelationp'],
     'exclude'   => true,
     'inputType' => 'checkbox',
     'options'   => ['create', 'delete'],

src/Resources/contao/dca/tl_user_group.php
@@ -22,14 +22,27 @@
  */
 
 Contao\CoreBundle\DataContainer\PaletteManipulator::create()
-    ->addField(['newstagsp'], 'news_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    ->addField(
+        ['newstagsp', 'newstagsrelationp'],
+        'news_legend',
+        Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND
+    )
     ->applyToPalette('default', 'tl_user_group');
 
 /*
  * Add fields to tl_user_group
  */
 
-$GLOBALS['TL_DCA']['tl_user_group']['fields']['newstagsp'] = [
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['newstagsp']         = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_user']['newstagsp'],
+    'exclude'   => true,
+    'inputType' => 'checkbox',
+    'options'   => ['create', 'delete'],
+    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+    'eval'      => ['multiple' => true],
+    'sql'       => 'blob NULL'
+];
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['newstagsrelationp'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_user']['newstagsp'],
     'exclude'   => true,
     'inputType' => 'checkbox',

src/Resources/contao/languages/de/modules.php
@@ -17,4 +17,5 @@
  * @filesource
  */
 
-$GLOBALS['TL_LANG']['MOD']['tl_news_tags'] = 'Auszeichnungen';
+$GLOBALS['TL_LANG']['MOD']['tl_news_tags']          = 'Auszeichnungen';
+$GLOBALS['TL_LANG']['MOD']['tl_news_tags_relation'] = 'Verbindungen';

src/Resources/contao/languages/de/tl_user.php
@@ -21,4 +21,5 @@
  * Permissions.
  */
 
-$GLOBALS['TL_LANG']['tl_user']['newstagsp'] = ['Auszeichnungsrechte', 'Hier können Sie die Auszeichnungsrechte festlegen.'];
+$GLOBALS['TL_LANG']['tl_user']['newstagsp']         = ['Auszeichnungsrechte', 'Hier können Sie die Auszeichnungsrechte festlegen.'];
+$GLOBALS['TL_LANG']['tl_user']['newstagsrelationp'] = ['Verbindungsrechte', 'Hier können Sie die Nachrichtenverbindungsrechte festlegen.'];

src/Resources/contao/languages/en/modules.php
@@ -17,4 +17,5 @@
  * @filesource
  */
 
-$GLOBALS['TL_LANG']['MOD']['tl_news_tags'] = 'Tags';
+$GLOBALS['TL_LANG']['MOD']['tl_news_tags']          = 'Tags';
+$GLOBALS['TL_LANG']['MOD']['tl_news_tags_relation'] = 'Tags relation';

ContaoBlackForest/contao-calendar-tags-bundle

src/EventListener/Table/CalendarEventsTagsRelation/TagOptions.php
@@ -1,0 +1,80 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-calendar-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-calendar-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-calendar-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace BlackForest\Contao\Calendar\Tags\EventListener\Table\CalendarEventsTagsRelation;
+
+use Contao\DataContainer;
+use Doctrine\DBAL\Connection;
+
+/**
+ * This class handle the tag options.
+ */
+class TagOptions
+{
+    /**
+     * The database connection.
+     *
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * The constructor.
+     *
+     * @param Connection $connection The database connection.
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Handle the tag options.
+     *
+     * @param DataContainer $container The data container.
+     *
+     * @return array
+     */
+    public function handle(DataContainer $container)
+    {
+        if (!$container->activeRecord->calendar || !$container->activeRecord->calendarEvents) {
+            return [];
+        }
+
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder
+            ->select('t.id, t.title')
+            ->from('tl_calendar_events_tags', 't')
+            ->where($queryBuilder->expr()->like('t.calendar', ':calendar'))
+            ->setParameter(':calendar', '%"' . $container->activeRecord->calendar . '"%')
+            ->orderBy('t.title');
+
+        $statement = $queryBuilder->execute();
+        if (!$statement->rowCount()) {
+            return [];
+        }
+
+        $options = [];
+        foreach ($statement->fetchAll(\PDO::FETCH_OBJ) as $tag) {
+            $options[$tag->id] = $tag->title;
+        }
+
+        return $options;
+    }
+}

src/Resources/contao/dca/tl_user.php
@@ -22,7 +22,11 @@
  */
 
 Contao\CoreBundle\DataContainer\PaletteManipulator::create()
-    ->addField(['calendareventstagsp'], 'calendars_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    ->addField(
+        ['calendareventstagsp', 'calendareventstagsrelationp'],
+        'calendars_legend',
+        Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND
+    )
     ->applyToPalette('extend', 'tl_user')
     ->applyToPalette('custom', 'tl_user');
 
@@ -30,8 +34,17 @@ Contao\CoreBundle\DataContainer\PaletteManipulator::create()
  * Add fields to tl_user_group
  */
 
-$GLOBALS['TL_DCA']['tl_user']['fields']['calendareventstagsp'] = [
+$GLOBALS['TL_DCA']['tl_user']['fields']['calendareventstagsp']         = [
     'label'     => &$GLOBALS['TL_LANG']['tl_user']['calendareventstagsp'],
+    'exclude'   => true,
+    'inputType' => 'checkbox',
+    'options'   => ['create', 'delete'],
+    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+    'eval'      => ['multiple' => true],
+    'sql'       => 'blob NULL'
+];
+$GLOBALS['TL_DCA']['tl_user']['fields']['calendareventstagsrelationp'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_user']['calendareventstagsrelationp'],
     'exclude'   => true,
     'inputType' => 'checkbox',
     'options'   => ['create', 'delete'],

src/Resources/contao/dca/tl_user_group.php
@@ -22,14 +22,27 @@
  */
 
 Contao\CoreBundle\DataContainer\PaletteManipulator::create()
-    ->addField(['calendareventstagsp'], 'calendars_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    ->addField(
+        ['calendareventstagsp', 'calendareventstagsrelationp'],
+        'calendars_legend',
+        Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND
+    )
     ->applyToPalette('default', 'tl_user_group');
 
 /*
  * Add fields to tl_user_group
  */
 
-$GLOBALS['TL_DCA']['tl_user_group']['fields']['calendareventstagsp'] = [
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['calendareventstagsp']         = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_user']['calendareventstagsp'],
+    'exclude'   => true,
+    'inputType' => 'checkbox',
+    'options'   => ['create', 'delete'],
+    'reference' => &$GLOBALS['TL_LANG']['MSC'],
+    'eval'      => ['multiple' => true],
+    'sql'       => 'blob NULL'
+];
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['calendareventstagsrelationp'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_user']['calendareventstagsp'],
     'exclude'   => true,
     'inputType' => 'checkbox',

src/Resources/contao/languages/de/modules.php
@@ -17,4 +17,5 @@
  * @filesource
  */
 
-$GLOBALS['TL_LANG']['MOD']['tl_calendar_events_tags'] = 'Auszeichnungen';
+$GLOBALS['TL_LANG']['MOD']['tl_calendar_events_tags']          = 'Auszeichnungen';
+$GLOBALS['TL_LANG']['MOD']['tl_calendar_events_tags_relation'] = 'Verbindungen';

src/Resources/contao/languages/de/tl_user.php
@@ -21,4 +21,5 @@
  * Permissions.
  */
 
-$GLOBALS['TL_LANG']['tl_user']['calendareventstagsp'] = ['Auszeichnungsrechte', 'Hier können Sie die Auszeichnungsrechte festlegen.'];
+$GLOBALS['TL_LANG']['tl_user']['calendareventstagsp']         = ['Auszeichnungsrechte', 'Hier können Sie die Auszeichnungsrechte festlegen.'];
+$GLOBALS['TL_LANG']['tl_user']['calendareventstagsrelationp'] = ['Verbindungsrechte', 'Hier können Sie die Verbindungsrechte der Events festlegen.'];

src/Resources/contao/languages/en/modules.php
@@ -17,4 +17,5 @@
  * @filesource
  */
 
-$GLOBALS['TL_LANG']['MOD']['tl_calendar_events_tags'] = 'Tags';
+$GLOBALS['TL_LANG']['MOD']['tl_calendar_events_tags']          = 'Tags';
+$GLOBALS['TL_LANG']['MOD']['tl_calendar_events_tags_relation'] = 'Tags relation';