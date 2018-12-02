ContaoBlackForest/contao-news-tags-bundle

Filter tag their has relation with the news by tags option

src/EventListener/Table/NewsTagsRelation/TagOptions.php
@@ -71,8 +71,14 @@ class TagOptions
             return [];
         }
 
+        $match = $this->matchNewsRelation($container);
+
         $options = [];
         foreach ($statement->fetchAll(\PDO::FETCH_OBJ) as $tag) {
+            if (\in_array($tag->id, $match)) {
+                continue;
+            }
+
             $options[$tag->id] = $tag->title;
         }
 
@@ -96,5 +102,38 @@ class TagOptions
         $queryBuilder
             ->where($queryBuilder->expr()->like('nt.archives', ':archives'))
             ->setParameter(':archives', '%"' . $container->activeRecord->archive . '"%');
+    }
+
+    /**
+     * Match news relation.
+     *
+     * @param DataContainer $container The data container.
+     *
+     * @return array
+     */
+    private function matchNewsRelation(DataContainer $container)
+    {
+        if (!$container->activeRecord->id && !$container->activeRecord->archive && !$container->activeRecord->news) {
+            return [];
+        }
+
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder
+            ->select('r.*')
+            ->from('tl_news_tags_relation', 'r')
+            ->where($queryBuilder->expr()->eq('r.news', ':newsId'))
+            ->setParameter(':newsId', $container->activeRecord->news);
+
+        $statement = $queryBuilder->execute();
+        if (!$statement->rowCount()) {
+            return [];
+        }
+
+        $match = [];
+        foreach ($statement->fetchAll(\PDO::FETCH_OBJ) as $relation) {
+            $match[] = $relation->tag;
+        }
+
+        return $match;
     }
 }

ContaoBlackForest/contao-calendar-tags-bundle

src/EventListener/Table/CalendarEventsTagsRelation/TagOptions.php
@@ -74,8 +74,16 @@ class TagOptions
             return [];
         }
 
+        $match = $this->matchEventsRelation($container);
+
         $options = [];
         foreach ($statement->fetchAll(\PDO::FETCH_OBJ) as $tag) {
+            if (\in_array($tag->id, $match)
+                && ((int) $tag->id !== (int) $container->activeRecord->tag)
+            ) {
+                continue;
+            }
+
             $options[$tag->id] = $tag->title;
         }
 
@@ -101,6 +109,44 @@ class TagOptions
 
         $queryBuilder
             ->where($queryBuilder->expr()->like('t.calendar', ':calendar'))
-            ->setParameter(':calendar', '%"' . $container->activeRecord->calendar . '"%');
+            ->orWhere($queryBuilder->expr()->eq('t.id', ':activeTag'))
+            ->setParameter(':calendar', '%"' . $container->activeRecord->calendar . '"%')
+            ->setParameter(':activeTag', $container->activeRecord->tag);
+    }
+
+    /**
+     * Match calendar events relation.
+     *
+     * @param DataContainer $container The data container.
+     *
+     * @return array
+     */
+    private function matchEventsRelation(DataContainer $container)
+    {
+        if (!$container->activeRecord->id
+            && !$container->activeRecord->calendar
+            && !$container->activeRecord->calendarEvents
+        ) {
+            return [];
+        }
+
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder
+            ->select('r.*')
+            ->from('tl_calendar_events_tags_relation', 'r')
+            ->where($queryBuilder->expr()->eq('r.calendarEvents', ':calendarEventsId'))
+            ->setParameter(':calendarEventsId', $container->activeRecord->calendarEvents);
+
+        $statement = $queryBuilder->execute();
+        if (!$statement->rowCount()) {
+            return [];
+        }
+
+        $match = [];
+        foreach ($statement->fetchAll(\PDO::FETCH_OBJ) as $relation) {
+            $match[] = $relation->tag;
+        }
+
+        return $match;
     }
 }