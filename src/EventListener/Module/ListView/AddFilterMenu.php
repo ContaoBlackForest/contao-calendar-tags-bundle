ContaoBlackForest/contao-news-tags-bundle

So we can pre filter the news list with the tags

src/EventListener/Module/ListView/AddFilterMenu.php
@@ -125,7 +125,7 @@ class AddFilterMenu
             'urlSuffix' => $this->urlSuffix,
             'moduleId'  => $template->id,
             'tags'      => $tags,
-            'active'    => $this->input->get('filterTag')
+            'active'    => $this->determineActiveTag($tags, $template->newsTagsPreFilter)
         ];
 
         $filterTemplate = new FrontendTemplate('news_list_tags_filter');
@@ -134,6 +134,31 @@ class AddFilterMenu
         $template->articles = \array_merge([$filterTemplate->parse()], $template->articles);
 
         $this->addListViewToSession($data['pathInfo']);
+    }
+
+    /**
+     * Determine the active tag.
+     *
+     * @param array  $tags          The tag list.
+     * @param string $tagIdentifier The tag identifier.
+     *
+     * @return string
+     */
+    private function determineActiveTag(array $tags, $tagIdentifier)
+    {
+        if ($this->input->get('filterTag')) {
+            return $this->input->get('filterTag');
+        } elseif ($tagIdentifier) {
+            foreach ($tags as $tag) {
+                if ((int) $tagIdentifier !== (int) $tag->id) {
+                    continue;
+                }
+
+                return $tag->alias;
+            }
+        }
+
+        return '';
     }
 
     /**

src/Resources/contao/languages/de/tl_module.php
@@ -21,5 +21,6 @@
  * Fields.
  */
 
-$GLOBALS['TL_LANG']['tl_module']['newsTagsFilter'] = ['Auszeichnungen filtern', 'Hier können Sie den Filter für die Auszeichnungen aktivieren. Wenn der Filter aktiviert ist, kann man die Nachrichten nach den Auszeichnungen filtern.'];
-$GLOBALS['TL_LANG']['tl_module']['newsTagsShow']   = ['Auszeichnungen anzeigen', 'Hier können Sie die Option aktvieren, damit die Auszeichnungen auf der Detailseite mit ausgeben werden.'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsFilter']    = ['Auszeichnungen filtern', 'Hier können Sie den Filter für die Auszeichnungen aktivieren. Wenn der Filter aktiviert ist, kann man die Nachrichten nach den Auszeichnungen filtern.'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsPreFilter'] = ['Auszeichnungen vorfiltern', 'Hier kann ausgewählt werden, mit welchen Auszeichnungen die Liste vorab gefiltert wird..'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsShow']      = ['Auszeichnungen anzeigen', 'Hier können Sie die Option aktvieren, damit die Auszeichnungen auf der Detailseite mit ausgeben werden.'];

src/Resources/contao/languages/en/tl_module.php
@@ -21,5 +21,6 @@
  * Fields.
  */
 
-$GLOBALS['TL_LANG']['tl_module']['newsTagsFilter'] = ['Tags filter', 'Activate the tags filter. If this active you can filter the news by tags.'];
-$GLOBALS['TL_LANG']['tl_module']['newsTagsShow']   = ['Show tags', 'Here can you activate for display the tags by the detail page.'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsFilter']    = ['Tags filter', 'Activate the tags filter. If this active you can filter the news by tags.'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsPreFilter'] = ['Pre filter Tags', 'Here can select with which tags the list is pre filtered.'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsShow']      = ['Show tags', 'Here can you activate for display the tags by the detail page.'];

ContaoBlackForest/contao-calendar-tags-bundle

src/EventListener/Module/ListView/AddFilterMenu.php
@@ -115,7 +115,7 @@ class AddFilterMenu
             'urlSuffix' => $this->urlSuffix,
             'moduleId'  => $template->id,
             'tags'      => $tags,
-            'active'    => $this->input->get('filterTag')
+            'active'    => $this->determineActiveTag($tags, $template->calendarEventsTagsPreFilter)
         ];
 
         $filterTemplate = new FrontendTemplate('calendar_events_list_tags_filter');
@@ -124,6 +124,31 @@ class AddFilterMenu
         $template->events = $filterTemplate->parse(). PHP_EOL .  $template->events;
 
         $this->addListViewToSession($data['pathInfo']);
+    }
+
+    /**
+     * Determine the active tag.
+     *
+     * @param array  $tags          The tag list.
+     * @param string $tagIdentifier The tag identifier.
+     *
+     * @return string
+     */
+    private function determineActiveTag(array $tags, $tagIdentifier)
+    {
+        if ($this->input->get('filterTag')) {
+            return $this->input->get('filterTag');
+        } elseif ($tagIdentifier) {
+            foreach ($tags as $tag) {
+                if ((int) $tagIdentifier !== (int) $tag->id) {
+                    continue;
+                }
+
+                return $tag->alias;
+            }
+        }
+
+        return '';
     }
 
     /**

src/Resources/contao/languages/de/tl_module.php
@@ -21,5 +21,6 @@
  * Fields.
  */
 
-$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsFilter'] = ['Auszeichnungen filtern', 'Hier können Sie den Filter für die Auszeichnungen aktivieren. Wenn der Filter aktiviert ist, kann man die Events nach den Auszeichnungen filtern.'];
-$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsShow']   = ['Auszeichnungen anzeigen', 'Hier können Sie die Option aktvieren, damit die Auszeichnungen auf der Detailseite mit ausgeben werden.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsFilter']    = ['Auszeichnungen filtern', 'Hier können Sie den Filter für die Auszeichnungen aktivieren. Wenn der Filter aktiviert ist, kann man die Events nach den Auszeichnungen filtern.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsPreFilter'] = ['Auszeichnungen vorfiltern', 'Hier kann ausgewählt werden, mit welchen Auszeichnungen die Liste vorab gefiltert wird.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsShow']      = ['Auszeichnungen anzeigen', 'Hier können Sie die Option aktvieren, damit die Auszeichnungen auf der Detailseite mit ausgeben werden.'];

src/Resources/contao/languages/en/tl_module.php
@@ -21,5 +21,6 @@
  * Fields.
  */
 
-$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsFilter'] = ['Tags filter', 'Activate the tags filter. If this active you can filter the events by tags.'];
-$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsShow']   = ['Show tags', 'Here can you activate for display the tags by the detail page.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsFilter']    = ['Tags filter', 'Activate the tags filter. If this active you can filter the events by tags.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsPreFilter'] = ['Pre filter Tags', 'Here can select with which tags the list is pre filtered.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsShow']      = ['Show tags', 'Here can you activate for display the tags by the detail page.'];