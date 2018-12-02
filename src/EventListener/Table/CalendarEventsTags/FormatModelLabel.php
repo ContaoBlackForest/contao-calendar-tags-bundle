ContaoBlackForest/contao-news-tags-bundle

Add the frontend integration

src/EventListener/Table/NewsTags/FormatModelLabel.php
@@ -68,7 +68,7 @@ class FormatModelLabel
         }
 
         $label  = '<p>' . $GLOBALS['TL_LANG']['MOD']['tl_news_tags'] . ': ';
-        $label .= '<br>&nbsp;' . $row['title'] . '</p>';
+        $label .= '<br>&nbsp;&nbsp;' . $row['title'] . '</p>';
 
         $archiveNames = $this->fetchNewsArchiveNames($row['archives']);
 
@@ -86,6 +86,12 @@ class FormatModelLabel
             $label .= '</ul>';
         }
         $label .= '</p>';
+
+        if ($row['tagLink']) {
+            $page   = $this->fetchPageById($row['tagLinkFallback']);
+            $label .= '<p>' . $GLOBALS['TL_LANG']['tl_news_tags']['tagLinkFallback'][0] . ': ';
+            $label .= '<br>&nbsp;&nbsp;' . $page->title . '</p>';
+        }
 
         return $label;
     }
@@ -121,5 +127,29 @@ class FormatModelLabel
         }
 
         return $archiveNames;
+    }
+
+    /**
+     * Fetch the page by id.
+     *
+     * @param string $pageId The page id.
+     *
+     * @return mixed|null
+     */
+    private function fetchPageById($pageId)
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder
+            ->select('p.*')
+            ->from('tl_page', 'p')
+            ->where($queryBuilder->expr()->eq('p.id', ':pageId'))
+            ->setParameter(':pageId', $pageId);
+
+        $statement = $queryBuilder->execute();
+        if (!$statement->rowCount()) {
+            return null;
+        }
+
+        return $statement->fetch(\PDO::FETCH_OBJ);
     }
 }

src/Resources/contao/languages/de/tl_module.php
@@ -1,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-news-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-news-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-news-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+/*
+ * Fields.
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['newsTagsFilter'] = ['Auszeichnungen filtern', 'Hier können Sie den Filter für die Auszeichnungen aktivieren. Wenn der Filter aktiviert ist, kann man die Nachrichten nach den Auszeichnungen filtern.'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsShow']   = ['Auszeichnungen anzeigen', 'Hier können Sie die Option aktvieren, damit die Auszeichnungen auf der Detailseite mit ausgeben werden.'];

src/Resources/contao/languages/en/default.php
@@ -1,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-news-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-news-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-news-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['FMD']['newsTagsShow']['show'] = 'Tags';
+
+$GLOBALS['TL_LANG']['FMD']['newsTagsFilter']['filter']      = 'Filter';
+$GLOBALS['TL_LANG']['FMD']['newsTagsFilter']['clearFilter'] = 'Clear Filter';

src/Resources/contao/languages/en/tl_module.php
@@ -1,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-news-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-news-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-news-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+/*
+ * Fields.
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['newsTagsFilter'] = ['Tags filter', 'Activate the tags filter. If this active you can filter the news by tags.'];
+$GLOBALS['TL_LANG']['tl_module']['newsTagsShow']   = ['Show tags', 'Here can you activate for display the tags by the detail page.'];

ContaoBlackForest/contao-calendar-tags-bundle

src/EventListener/Table/CalendarEventsTags/FormatModelLabel.php
@@ -68,7 +68,7 @@ class FormatModelLabel
         }
 
         $label  = '<p>' . $GLOBALS['TL_LANG']['MOD']['tl_calendar_events_tags'] . ': ';
-        $label .= '<br>&nbsp;' . $row['title'] . '</p>';
+        $label .= '<br>&nbsp;&nbsp;' . $row['title'] . '</p>';
 
         $calendarNames = $this->fetchCalendarNames($row['calendar']);
 
@@ -86,6 +86,12 @@ class FormatModelLabel
             $label .= '</ul>';
         }
         $label .= '</p>';
+
+        if ($row['tagLink'] && $row['tagLinkFallback']) {
+            $page   = $this->fetchPageById($row['tagLinkFallback']);
+            $label .= '<p>' . $GLOBALS['TL_LANG']['tl_calendar_events_tags']['tagLinkFallback'][0] . ': ';
+            $label .= '<br>&nbsp;&nbsp;' . $page->title . '</p>';
+        }
 
         return $label;
     }
@@ -125,5 +131,29 @@ class FormatModelLabel
         }
 
         return $calendarNames;
+    }
+
+    /**
+     * Fetch the page by id.
+     *
+     * @param string $pageId The page id.
+     *
+     * @return mixed|null
+     */
+    private function fetchPageById($pageId)
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder
+            ->select('p.*')
+            ->from('tl_page', 'p')
+            ->where($queryBuilder->expr()->eq('p.id', ':pageId'))
+            ->setParameter(':pageId', $pageId);
+
+        $statement = $queryBuilder->execute();
+        if (!$statement->rowCount()) {
+            return null;
+        }
+
+        return $statement->fetch(\PDO::FETCH_OBJ);
     }
 }

src/Resources/contao/languages/de/tl_module.php
@@ -1,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-calendar-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-calendar-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-calendar-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+/*
+ * Fields.
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsFilter'] = ['Auszeichnungen filtern', 'Hier können Sie den Filter für die Auszeichnungen aktivieren. Wenn der Filter aktiviert ist, kann man die Events nach den Auszeichnungen filtern.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsShow']   = ['Auszeichnungen anzeigen', 'Hier können Sie die Option aktvieren, damit die Auszeichnungen auf der Detailseite mit ausgeben werden.'];

src/Resources/contao/languages/en/default.php
@@ -1,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-calendar-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-calendar-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-calendar-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['FMD']['calendarEventsTagsShow']['show'] = 'Tags';
+
+$GLOBALS['TL_LANG']['FMD']['calendarEventsTagsFilter']['filter']      = 'Filter';
+$GLOBALS['TL_LANG']['FMD']['calendarEventsTagsFilter']['clearFilter'] = 'Clear Filter';

src/Resources/contao/languages/en/tl_module.php
@@ -1,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-calendar-tags-bundle.
+ *
+ * (c) 2014-2018 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-calendar-tags-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-calendar-tags-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+/*
+ * Fields.
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsFilter'] = ['Tags filter', 'Activate the tags filter. If this active you can filter the events by tags.'];
+$GLOBALS['TL_LANG']['tl_module']['calendarEventsTagsShow']   = ['Show tags', 'Here can you activate for display the tags by the detail page.'];